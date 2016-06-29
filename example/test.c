d3031f29ce7bb4c72639c4458ed880c1e426465c
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-06-29 15:27:35 +0200

    create a folder with exmples

---
+++ example/test.c
@@ -0,0 +1,14 @@
+#include <hmon.h>
+
+int main(){
+    monitor_lib_init(NULL, NULL, "output_test");
+    monitors_import("./example_monitor");
+    monitors_start();
+    monitors_update();
+    sleep(1);
+    monitors_update();
+    monitors_output(monitor_output, 1);
+    monitor_lib_finalize();
+    return 0;
+}
+
