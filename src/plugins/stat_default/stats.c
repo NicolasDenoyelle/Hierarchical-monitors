626c72c0c155e7ccaa8157ed5048192aaa120e43
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-07-25 23:12:40 +0200

    normalization error corrected

--- src/plugins/stat_default/stats.c
+++ src/plugins/stat_default/stats.c
@@ -29,4 +29,16 @@ double monitor_evset_sum(struct monitor * monitor){
     return result;
 }
 
+double monitor_normalized_evset_sum(struct monitor * monitor){
+    double result = 0;
+    unsigned c = monitor->current, n = monitor->n_events;
+    while(n--){
+	if(monitor->events_min[n]!=monitor->events_max[n]){
+	    double range = monitor->events_max[n] - monitor->events_min[n];
+	    result+=(monitor->events[c][n] - monitor->events_min[n])/range;
+	} else {result += 1;}
+    }
+    return result/(double)(monitor->n_events);
+}
+
 
