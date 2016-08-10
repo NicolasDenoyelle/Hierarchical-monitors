83fae2b8941c076fafed912c13a1339d5cbe548f
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-08-10 11:55:26 +0200

    output scale when performing normalization

--- src/plugins/learning/features.c
+++ src/plugins/learning/features.c
@@ -7,25 +7,38 @@ gsl_vector * gsl_vector_dup(const gsl_vector * v){
     return copy;
 }
 
-void gsl_vector_normalize(gsl_vector * v){
-    double max  = gsl_vector_max(v);
-    double min = gsl_vector_min(v);
-    double mu = gsl_stats_mean(v->data, v->stride, v->size);
-    gsl_vector_add_constant(v, -mu);
-    if(max!=min){gsl_vector_scale(v,1/(max-min));}
+struct scaling gsl_vector_normalize(gsl_vector * v){
+    struct scaling scale = {gsl_vector_max(v) - gsl_vector_min(v), gsl_stats_mean(v->data, v->stride, v->size)};
+    gsl_vector_add_constant(v, -scale.center);
+    if(scale.scale!=0){gsl_vector_scale(v,1/scale.scale);}
+    return scale;
 }
 
-void gsl_matrix_normalize_columns(gsl_matrix * mat){
-    for(unsigned i =0; i<mat->size2; i++){
-	gsl_vector_view col = gsl_matrix_column (mat, i);
-	gsl_vector_normalize(&col.vector);
+void gsl_matrix_normalize_columns(gsl_matrix * mat, struct scaling * scales){
+    if(scales == NULL){
+	for(unsigned i =0; i<mat->size2; i++){
+	    gsl_vector_view col = gsl_matrix_column (mat, i);
+	    gsl_vector_normalize(&col.vector);
+	}
+    } else {
+	for(unsigned i =0; i<mat->size2; i++){
+	    gsl_vector_view col = gsl_matrix_column (mat, i);
+	    scales[i] = gsl_vector_normalize(&col.vector);
+	}
     }
 }
 
-void gsl_matrix_normalize_rows(gsl_matrix * mat){
-    for(unsigned i =0; i<mat->size2; i++){
-	gsl_vector_view row = gsl_matrix_row(mat, i);
-	gsl_vector_normalize(&row.vector);
+void gsl_matrix_normalize_rows(gsl_matrix * mat, struct scaling * scales){
+    if(scales == NULL){
+	for(unsigned i =0; i<mat->size2; i++){
+	    gsl_vector_view row = gsl_matrix_row(mat, i);
+	    gsl_vector_normalize(&row.vector);
+	}
+    } else {
+	for(unsigned i =0; i<mat->size2; i++){
+	    gsl_vector_view row = gsl_matrix_row(mat, i);
+	    scales[i] = gsl_vector_normalize(&row.vector);
+	}
     }
 }
 
