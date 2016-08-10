859f4b0a5e6f8547768cfb1a19a9fbec68a73698
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-08-10 19:05:15 +0200

    discard infinite values

--- utils/hmon_plot.R
+++ utils/hmon_plot.R
@@ -95,7 +95,7 @@ plot_monitor = function(frame, ymin = NULL, ymax = NULL, xmin = NULL, xmax = NUL
 
 plot_monitors <- function(frame){
   par(ann=FALSE)
-  frame = frame[is.finite(frame[, options$column]),]
+  frame = frame[is.finite(rowSums(frame[, options$column:ncol(frame)])),]
   if(!options$split){
     ymax = max(frame[, options$column],  na.rm = TRUE)
     ymin = min(frame[, options$column],  na.rm = TRUE)
