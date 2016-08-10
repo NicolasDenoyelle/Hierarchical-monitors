0026e3ee92f66a8d534b253ba34dd201d756b465
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-08-10 11:57:02 +0200

    output error, and remove columns normalization

--- src/plugins/learning/learning.c
+++ src/plugins/learning/learning.c
@@ -1,20 +1,16 @@
 #include "../../hmon.h"
 #include "learning.h"
 
-#define KMEAN_ITER 10
-#define K 2
-#define LAMBDA 100000
-
 /**
  * Perform K-mean clustering into K clusters on events, and output each event label.
  **/
 void clustering(hmatrix events, __attribute__ ((unused)) unsigned last, double * samples, unsigned n_samples, __attribute ((unused)) void ** userdata){
     /* Normalize events */
     gsl_matrix * normalized_points = to_gsl_matrix(events.data, events.rows, events.cols);
-    gsl_matrix_normalize_columns(normalized_points);
+    gsl_matrix_normalize_columns(normalized_points, NULL);
     
     /* centroids move */
-    centroids c = kmean(normalized_points, K, KMEAN_ITER);
+    centroids c = kmean(normalized_points, K);
     
     /**** Output points label */
     for(unsigned i=0; i<events.rows && i<n_samples; i++){samples[i] = gsl_vector_ulong_get(c->label, i);}
@@ -34,12 +30,11 @@ void lsq_fit(hmatrix events, __attribute__ ((unused)) unsigned last, double * sa
     if(last < events.rows-1){return;}
     
     /* Normalize events */
-    gsl_matrix * normalized_events = to_gsl_matrix(events.data, events.rows, events.cols);
-    gsl_matrix_normalize_columns(normalized_events);
-
+    gsl_matrix * matrix_events = to_gsl_matrix(events.data, events.rows, events.cols);
+    
     /* Extract features(events without timesteps and target) and target(first event) */
-    gsl_vector_const_view y = gsl_matrix_const_column(normalized_events, 0);
-    gsl_matrix_const_view X = gsl_matrix_const_submatrix(normalized_events, 0, 1, normalized_events->size1, n_samples-1);
+    gsl_vector_const_view y = gsl_matrix_const_column(matrix_events, 0);
+    gsl_matrix_const_view X = gsl_matrix_const_submatrix(matrix_events, 0, 1, events.rows, n_samples-1);
 
     /* Build the model */
     lm lm = *userdata;
@@ -48,7 +43,7 @@ void lsq_fit(hmatrix events, __attribute__ ((unused)) unsigned last, double * sa
     
     /* Output model parameters */
     for(unsigned i=1; i<n_samples; i++){samples[i] = gsl_vector_get(lm->Theta, i-1);}
-
+    
     /* Cleanup */
     /* delete_linear_model(lm); */
 }
