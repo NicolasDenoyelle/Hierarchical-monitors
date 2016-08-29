1a84897da36264051f6068d31ca1ea6defc7a4e0
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-08-29 11:13:04 +0200

    move sampling to an external file

---
+++ src/sampling.c
@@ -0,0 +1,78 @@
+#include <signal.h>
+#include <sys/time.h>
+#include "hmon.h"
+
+int handler_isset = 0;
+timer_t update_timer;
+
+timer_t display_timer;
+int display_arg;
+void (*display_function)(int);
+
+static void handler(int sig, siginfo_t *si, void *uc){
+  timer_t * timer = si->si_value.sival_ptr;
+  if(sig == SIGRTMIN){
+    if(*timer == update_timer){
+      hmon_update();
+    }
+    if(*timer == display_timer){
+      display_function(display_arg);
+    }
+  }
+}
+
+static int set_handler(){
+   /* register handler for timer signal */
+  struct sigaction sa;
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = handler;
+  sigemptyset(&sa.sa_mask);
+  if(sigaction(SIGRTMIN, &sa, NULL) == -1){perror("sigaction"); return -1;}	   
+  handler_isset = 1;
+  return 0;
+}
+
+static int create_timer(timer_t * timerid){
+/* Create the timer */
+  struct sigevent sev;
+  sev.sigev_notify = SIGEV_SIGNAL;
+  sev.sigev_signo = SIGRTMIN;
+  sev.sigev_value.sival_ptr = timerid;
+  if(timer_create(CLOCK_REALTIME, &sev, timerid) == -1){perror("timer_create"); return -1;}
+}
+
+static int set_timer(timer_t timer, long us){
+  struct itimerspec update_delay;
+  update_delay.it_interval.tv_sec  = us/1000;
+  update_delay.it_interval.tv_nsec = 1000000*(us%1000);
+  update_delay.it_value = update_delay.it_interval;
+  if(timer_settime(timer, 0, &update_delay, NULL) == -1){perror("timer_settime"); return -1;}
+  return 0;
+}
+
+int hmon_sampling_start(const long us){
+  if(!handler_isset && set_handler() == -1){return -1;}
+  if(create_timer(&update_timer) == -1){return -1;}
+  return set_timer(update_timer, us);
+}
+
+int hmon_sampling_stop(){
+  set_timer(update_timer, 0);
+  if(timer_delete(update_timer) == -1){perror("timer_delete"); return -1;}
+  return 0;
+}
+
+int hmon_periodic_display_start(void (*display_monitors)(int), int arg){
+  display_function = display_monitors;
+  display_arg = arg;
+  if(!handler_isset && set_handler() == -1){return -1;}
+  if(create_timer(&display_timer) == -1){return -1;}
+  return set_timer(display_timer, 1000);
+}
+
+int hmon_periodic_display_stop(){
+  set_timer(display_timer, 0);
+  if(timer_delete(display_timer) == -1){perror("timer_delete"); return -1;}
+  return 0;
+}
+
