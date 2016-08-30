a6e1aa8be89ef4523006f7cf44888832ae293a27
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-08-30 15:26:14 +0200

    output trace to /dev/null

--- src/hmon_debug.sh
+++ src/hmon_debug.sh
@@ -2,5 +2,5 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/plugins/papi:$PWD/plugins/maqao:$PWD/plugins/learning:$PWD/plugins/stat_default:$PWD/plugins/fake:$PWD/plugins/accumulate:$PWD/plugins/hierarchical
 
-libtool --mode=execute gdb --eval-command "handle SIG34 pass noprint nostop" --eval-command "handle SIGINT pass" --args ./hmonitor --input ../example/example_monitor -f 100000 -d
+libtool --mode=execute gdb --eval-command "handle SIG34 pass noprint nostop" --eval-command "handle SIGINT pass" --args ./hmonitor --input ../example/example_monitor -f 100000 -d -o /dev/null
 
