5ee1136cd677d1c43b6cab1b5032039d06ff8760
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-12-15 13:27:06 +0100

    change output to have one trace per monitor id

--- src/hmon_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/plugins/papi:$PWD/plugins/maqao:$PWD/plugins/learning:$PWD/plugins/stat_default:$PWD/plugins/fake:$PWD/plugins/accumulate:$PWD/plugins/hierarchical
-
-libtool --mode=execute gdb --eval-command "handle SIG34 pass noprint nostop" --eval-command "handle SIGINT pass" --args ./hmonitor --input ../example/example_monitor -f 100000 -d -o /dev/null
-
